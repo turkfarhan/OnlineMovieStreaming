c79ae04033534911237b92f89e222bf8d2b3bd0f
farhanpasha2000@gmail.com <farhanpasha2000@gmail.com>
2021-02-06 09:51:32 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# OnlineMovieStreaming
+
+
+
+# Functionalities : 
+           - login system : create acc, delete acc, update user details
+           - watching movies/videos
+           - latest updated videos
+           - most viewed videos
+           - search functionality
+* the one to signup first is the admin
+* only the admin has the rights to upload movies/videos and images.
+
+
+# Technologies used :
+* HTML, CSS, Bootstrap for frontend
+* PHP and MySQL for backend
+
+# Before use :
+
+- truncate all the tables after importing them from the sql-files folder & edit the database name in dbh.php
+- create a "uploads" folder to store the uploaded images/posters of the movie/video.
+- create a "video-uploads" folder to store uploaded videos of the corresponding image.
+
+### Note:
+`Only lets you upload videos upto 2mb.
+Changes should be made in configuration file for larger files.`
+
+# Screenshots :
+
+**Homepage**
+![](scrshots/1.jpg)
+
+**Admin page**
+![](scrshots/2.jpg)
+
+**Add movie**
+![](scrshots/3.jpg)
+
+**Movie page**
+![](scrshots/5.jpg)
+
+**Account details**
+![](scrshots/4.jpg)
---
+++ sql-files/movies.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 20, 2018 at 01:19 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movies`
+--
+
+CREATE TABLE `movies` (
+  `mid` int(10) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `genre` varchar(20) NOT NULL,
+  `rdate` varchar(5) NOT NULL,
+  `runtime` varchar(4) NOT NULL,
+  `decription` varchar(100) NOT NULL,
+  `viewers` int(10) DEFAULT '1',
+  `imgpath` varchar(50) NOT NULL,
+  `videopath` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `movies`
+--
+ALTER TABLE `movies`
+  ADD PRIMARY KEY (`mid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `movies`
+--
+ALTER TABLE `movies`
+  MODIFY `mid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
